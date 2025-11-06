ce9ff377f702eb110cb774af4602047bae2c2fd5
silly-d <cjudmd2018@gmail.com>
2025-11-06 20:51:05 +0900

    Revert "Add files via upload"
    This reverts commit 2efb46315f546641ec9bc520253fdb9aff91111d.

--- js/profile.js
+++
@@ -1,8 +0,0 @@
-// ---------- scroll down button ----------
-document.querySelector('.field').addEventListener('click', function() {
-    document.querySelector('.content02').scrollIntoView({ behavior: 'smooth' });
-});
-
-AOS.init({
-    once: false, // 다시 스크롤될 때마다 애니메이션 재생
-});
